9b2cda6e2a076f43022e907666f55526eb89a1d9
Scorpion <btejanshu@gmail.com>
2024-12-12 12:15:53 +0530

    lab2 3rd program

---
+++ LAB_02/p3.py
@@ -0,0 +1,27 @@
+#given a and b find the number of possible values that x can take , so that the equation a mod x =b
+def check(a,b):
+    count=0
+    p = []
+    for x in range(1,a+1):
+        if(a%x==b):
+            count += 1
+            p.append(x)
+        else:
+            continue
+    return count,p
+
+def main():
+    a=int(input("Enter a number "))
+    b=int(input("enter another number "))
+    
+    k,l=check(a,b)
+    
+    
+    print("Number of values satisfying the condition a%x =b are",k)
+    
+    for x in l:
+        print(x)
+
+if __name__=="__main__":
+    main()
+        
