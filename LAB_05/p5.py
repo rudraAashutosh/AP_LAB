46cf1ce1f2d78f8d2a33cc6265d6340ec31e2b68
Scorpion <btejanshu@gmail.com>
2025-01-16 13:20:44 +0530

    edited question 5

--- LAB_05/p5.py
+++ LAB_05/p5.py
@@ -1,14 +1,52 @@
-def height(root):
+def create_node(value):
+    return {"value": value, "left": None, "right": None}
+
+
+def insert_into_bst(root, value):
     if root is None:
-        return 0
-    return max(height(root.left), height(root.right)) + 1
-  
-def isBalanced(root):
-    if root is None:
-        return True
-    lh = height(root.left)
-    rh = height(root.right)
-    if (abs(lh - rh) <= 1) and isBalanced(
-            root.left) is True and isBalanced(root.right) is True:
-        return True
-    return False
+        return create_node(value)
+    
+    if value < root["value"]:
+        root["left"] = insert_into_bst(root["left"], value)
+    else:
+        root["right"] = insert_into_bst(root["right"], value)
+    
+    return root
+
+
+def construct_bst(arr):
+    root = None
+    for value in arr:
+        root = insert_into_bst(root, value)
+    return root
+
+
+def inorder(root, k, result):
+    if root is None or result[0] >= k:
+        return None
+
+    value = inorder(root["right"], k, result)
+    if value is not None:
+        return value
+
+
+    result[0] += 1
+    if result[0] == k:
+        return root["value"]
+
+
+    return inorder(root["left"], k, result)
+
+
+def k_largest_element(root, k):
+    result = [0]
+    return inorder(root, k, result)
+
+
+arr = [8, 3, 10, 1, 6, 14, 4, 7, 13]
+bst_root = construct_bst(arr)
+k = 3
+
+print(f"The {k}-th largest element in the BST is:")
+largest = k_largest_element(bst_root, k)
+print(largest)
